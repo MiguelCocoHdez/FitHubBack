77083d77977d4a3a18a7a43135cee925ad431dc9
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-07-02 00:43:33 +0200

    feat: Creacion del servicio de rutinas

---
+++ microservice-rutinas/src/main/java/com/fithub/rutinas/MicroserviceRutinasApplication.java
@@ -0,0 +1,13 @@
+package com.fithub.rutinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRutinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRutinasApplication.class, args);
+	}
+
+}
---
+++ microservice-rutinas/src/test/java/com/fithub/rutinas/MicroserviceRutinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fithub.rutinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRutinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
