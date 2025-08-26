3db8deb375ab8f011dddaac16e12c55c67c4c7bc
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-08-26 21:55:30 +0200

    fix: Cambiado path variable por request param en asignar rutina para que todas las llamadas a la api sean iguales en parametros

--- microservice-rutinas/src/main/java/com/fithub/rutinas/rest/RutinaRestController.java
+++ microservice-rutinas/src/main/java/com/fithub/rutinas/rest/RutinaRestController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,8 +54,8 @@ public class RutinaRestController {
 		return ResponseEntity.ok(respuesta);
 	}
 	
-	@PostMapping("/asignarRutina/{clientId}/{id}")
-	ResponseEntity<Map<String, String>> asignarRutina(@PathVariable Long clientId, @PathVariable Long id) {
+	@PostMapping("/asignarRutina")
+	ResponseEntity<Map<String, String>> asignarRutina(@RequestParam Long clientId, @RequestParam Long id) {
 		Map<String, String> respuesta = new HashMap<>();
 		
 		rs.asignarRutina(clientId, id);
